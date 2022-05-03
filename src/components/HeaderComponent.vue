65a314fda0dcce1d3afff26e70a8b4859376be18
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-05-03 17:03:02 +0200

    Inital commit

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,75 @@
+<template>
+    <header id="SiteHeader">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col">
+                    <div class="logo">
+                        <img src="@/assets/img/dc-logo.png" alt="" class="img-fluid">
+                    </div>
+                </div>
+                <div class="col">
+                    <nav>
+                        <a href="#" v-for="(link, index) in navLinks" :key="index">{{link.link}}</a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: 'HeaderComponent',
+    data() {
+        return {
+            navLinks: [
+                {
+                    link: "Characters"
+                },
+                {
+                    link: "Comics"
+                },
+                {
+                    link: "Movies"
+                },
+                {
+                    link: "TV"
+                },
+                {
+                    link: "Games"
+                },
+                {
+                    link: "Collectibles"
+                },
+                {
+                    link: "Videos"
+                },
+                {
+                    link: "Fans"
+                },
+                {
+                    link: "News"
+                },
+                {
+                    link: "Shop"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+    padding: 1rem;
+    a {
+        text-decoration: none;
+        text-transform: uppercase;
+        padding: 0.5rem;
+        font-size: 0.9rem;
+        font-weight: 600;
+        color: black;
+    }
+}
+</style>
