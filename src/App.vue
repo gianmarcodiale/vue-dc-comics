f431641cba976f3d4162712436bec68b2f918797
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-05-03 19:23:50 +0200

    add social section

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,23 @@
     </main>
     <SiteShopSection/>
     <SiteFooter/>
+    <SiteSocials/>
   </div>
 </template>
 
 <script>
 import SiteHeader from "@/components/HeaderComponent.vue";
-import SiteShopSection from "@/components/ShopSectionComponent.vue"
-import SiteFooter from "@/components/FooterComponent.vue"
+import SiteShopSection from "@/components/ShopSectionComponent.vue";
+import SiteFooter from "@/components/FooterComponent.vue";
+import SiteSocials from "@/components/SocialsComponent.vue"
 
 export default {
   name: 'App',
   components: {
     SiteHeader,
     SiteShopSection,
-    SiteFooter
+    SiteFooter,
+    SiteSocials
   }
 }
 </script>
---
+++ src/components/SocialsComponent.vue
@@ -0,0 +1,55 @@
+<template>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <button>SIGN UP NOW!</button>
+                </div>
+                <div class="social-links col d-flex align-items-center justify-content-end">
+                    <h4>FOLLOW US</h4>
+                    <img src="@/assets/img/footer-facebook.png" alt="" class="img-fluid">
+                    <img src="@/assets/img/footer-twitter.png" alt="" class="img-fluid">
+                    <img src="@/assets/img/footer-youtube.png" alt="" class="img-fluid">
+                    <img src="@/assets/img/footer-pinterest.png" alt="" class="img-fluid">
+                    <img src="@/assets/img/footer-periscope.png" alt="" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "SiteSocials"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/sass/_variables.scss";
+
+section {
+    background-color: $footerColor;
+    padding: 2rem;
+
+    button {
+        border: 1px solid $brandColor;
+        background-color: $footerColor;
+        color: white;
+        padding: 0.75rem;
+    }
+
+    h4 {
+        color: $brandColor;
+        margin-bottom: 0;
+        font-size: 1.25rem;
+    }
+
+    img {
+        width: 30px;
+    }
+
+    .social-links {
+        column-gap: 0.5rem;
+    }
+}
+</style>
