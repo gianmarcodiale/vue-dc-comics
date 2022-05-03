9afb5091db2da01d37df3a5fc257fb0f37f6d3af
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-05-03 19:06:41 +0200

    add site footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,8 +2,102 @@
     <footer>
         <div class="container">
             <div class="row">
-                <div class="col">
-
+                <div class="col footer-links">
+                    <div class="row">
+                        <div class="col">
+                            <h4>DC COMICS</h4>
+                            <ul>
+                                <li>
+                                    <a href="#">Characters</a>
+                                </li>
+                                <li>
+                                    <a href="#">Comics</a>
+                                </li>
+                                <li>
+                                    <a href="#">Movies</a>
+                                </li>
+                                <li>
+                                    <a href="#">TV</a>
+                                </li>
+                                <li>
+                                    <a href="#">Games</a>
+                                </li>
+                                <li>
+                                    <a href="#">Videos</a>
+                                </li>
+                                <li>
+                                    <a href="#">News</a>
+                                </li>
+                            </ul>
+                            <h4>SHOP</h4>
+                            <ul>
+                                <li>
+                                    <a href="#">Shop DC</a>
+                                </li>
+                                <li>
+                                    <a href="#">Shop DC Collectibles</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <h4>DC</h4>
+                            <ul>
+                                <li>
+                                    <a href="#">Terms Of Use</a>
+                                </li>
+                                <li>
+                                    <a href="#">Privacy policy</a>
+                                </li>
+                                <li>
+                                    <a href="#">Ad Choices</a>
+                                </li>
+                                <li>
+                                    <a href="#">Advertising</a>
+                                </li>
+                                <li>
+                                    <a href="#">Jobs</a>
+                                </li>
+                                <li>
+                                    <a href="#">Subscriptions</a>
+                                </li>
+                                <li>
+                                    <a href="#">Talent Workshops</a>
+                                </li>
+                                <li>
+                                    <a href="#">CPSC Certificates</a>
+                                </li>
+                                <li>
+                                    <a href="#">Ratings</a>
+                                </li>
+                                <li>
+                                    <a href="#">Shop Help</a>
+                                </li>
+                                <li>
+                                    <a href="#">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <h4>SITES</h4>
+                            <ul>
+                                <li>
+                                    <a href="#">DC</a>
+                                </li>
+                                <li>
+                                    <a href="#">MAD Magazine</a>
+                                </li>
+                                <li>
+                                    <a href="#">DC Kids</a>
+                                </li>
+                                <li>
+                                    <a href="#">DC Universe</a>
+                                </li>
+                                <li>
+                                    <a href="#">DC Power Visa</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
                 <div class="col">
                     <img src="@/assets/img/dc-logo-bg.png" alt="" class="img-fluid">
@@ -19,8 +113,29 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "@/assets/sass/_variables.scss";
+
 footer {
     background-image: url("@/assets/img/footer-bg.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    .footer-links {
+        padding: 4rem;
+    }
+
+    h4 {
+        color: white;
+    }
+
+    ul {
+        padding-inline-start: 0;
+    }
+
+    a {
+        text-decoration: none;
+        color: $footerTextColor;
+    }
 }
 </style>
